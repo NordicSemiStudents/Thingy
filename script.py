0c12dc6ad1343335a5340148b6f50b7d92e99d4e
Myrvold <mamy@nordicsemi.no>
2017-11-17 15:35:36 +0100

    added so the files are closed after reading and writing

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 sep = "power_manage();\n"
 with open ("blockCode.c") as file:
     blockCode = file.read()
+    file.close()
 with open("main.c") as file:
     firstPart, sep, secondPart = file.read().partition(sep)
+    file.close()
 with open("main.c", "w") as file:
     file.write(firstPart + sep + blockCode + secondPart)
+    file.close()
